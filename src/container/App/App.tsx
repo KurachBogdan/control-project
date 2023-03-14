9bac962ed47ae47979ccb3b0dc82e66f022e246d
Bohdan Kurach <kurach.link@gmail.com>
2023-03-14 17:38:35 +0200

    add components App, Header, Main, Footer

---
+++ src/container/App/App.tsx
@@ -0,0 +1,21 @@
+
+import { StyledEngineProvider } from '@mui/material'
+import { CssBaseline } from '@mui/material'
+import Footer from 'container/Footer/Footer'
+import Header from 'container/Header/Header'
+import Main from 'container/Main/Main'
+
+type Props = {}
+
+const App = (props: Props) => {
+    return (
+        <StyledEngineProvider injectFirst>
+            <CssBaseline />
+            <Header />
+            <Main />
+            <Footer />
+        </ StyledEngineProvider>
+    )
+}
+
+export default App
