7059b927cc5c137f9ced48894a193adcdb4655eb
Bohdan Kurach <kurach.link@gmail.com>
2023-03-18 01:18:21 +0200

    add articleArray

--- src/components/ArticleList/ArticleList.tsx
+++ src/components/ArticleList/ArticleList.tsx
@@ -1,52 +1,19 @@
 import ArticleListItem from './ArticleListItem'
 import './ArticleList.scss'
 import { Grid } from '@mui/material'
+import articleArray from 'utils/articleArray'
 
 type Props = {}
 
 const ArticleList = (props: Props) => {
     return (
         <Grid container className="article_list">
-            <Grid item md={5.7}>
-                {' '}
-                <ArticleListItem category="Tourism" />
-            </Grid>
-            <Grid item md={5.7}>
-                {' '}
-                <ArticleListItem category="Sport" />
-            </Grid>
-            <Grid item md={5.7}>
-                {' '}
-                <ArticleListItem category="Fashion" />
-            </Grid>
-            <Grid item md={5.7}>
-                {' '}
-                <ArticleListItem category="Clothes" />
-            </Grid>
-            <Grid item md={5.7}>
-                {' '}
-                <ArticleListItem category="Clothes" />
-            </Grid>
-            <Grid item md={5.7}>
-                {' '}
-                <ArticleListItem category="Fashion" />
-            </Grid>
-            <Grid item md={5.7}>
-                {' '}
-                <ArticleListItem category="Autumn" />
-            </Grid>
-            <Grid item md={5.7}>
-                {' '}
-                <ArticleListItem category="Clothes" />
-            </Grid>
-            <Grid item md={5.7}>
-                {' '}
-                <ArticleListItem category="Summer" />
-            </Grid>
-            <Grid item md={5.7}>
-                {' '}
-                <ArticleListItem category="Summer" />
-            </Grid>
+            {articleArray.map(({category}) => (
+                <Grid item md={5.7}>
+                    {' '}
+                    <ArticleListItem category={category} />
+                </Grid>
+            ))}
         </Grid>
     )
 }
---
+++ src/utils/articleArray.ts
@@ -0,0 +1,38 @@
+type ArticleProps = {
+    category: string
+}
+
+const articleArray:ArticleProps[] = [
+    {
+        category: 'Tourism'
+    },
+    {
+        category: 'Sport'
+    },
+    {
+        category: 'Fashion'
+    },
+    {
+        category: 'Clothes'
+    },
+    {
+        category: 'Clothes'
+    },
+    {
+        category: 'Fashion'
+    },
+    {
+        category: 'Autumn'
+    },
+    {
+        category: 'Clothes'
+    },
+    {
+        category: 'Summer'
+    },
+    {
+        category: 'Summer'
+    },
+]
+
+export default articleArray
