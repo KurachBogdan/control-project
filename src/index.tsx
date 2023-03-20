a0f1511b0d4ae11683437101544c7b94d44a3a3a
Bohdan Kurach <kurach.link@gmail.com>
2023-03-20 22:23:40 +0200

    add some styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import App from 'container/App/App';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import App from 'container/App/App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
