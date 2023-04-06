63dcb2533849ffa98fdfedcf8e99909886f24b75
Bohdan Kurach <kurach.link@gmail.com>
2023-04-07 02:37:35 +0300

    now liked articles is appearing on Favorite page

--- src/pages/Favorite/Favorite.tsx
+++ src/pages/Favorite/Favorite.tsx
@@ -1,7 +1,47 @@
+import ArticleListItem from 'components/ArticleList/ArticleListItem'
+import { Grid } from '@mui/material'
+import { useAppSelector } from 'redux/hooks'
+import articleArray from 'utils/articleArray'
+import Container from '@mui/material/Container/Container'
+
 type Props = {}
 
+type Article = {
+    id: number
+    category: string
+    image: string
+    title: string
+    content: string
+}
+
 const Favorite = (props: Props) => {
-    return <div>Favorite</div>
+    const likedArticleIds = useAppSelector((state) =>
+        Object.keys(state.articlesLikeState)
+            .filter((key) => state.articlesLikeState[+key])
+            .map((key) => parseInt(key))
+    )
+
+    const likedArticles: Article[] = articleArray.filter((article) =>
+        likedArticleIds.includes(article.id)
+    )
+
+    return (
+        <Container maxWidth="lg" className="home_container">
+            <Grid container className="article_list">
+                {likedArticles.map(({ id, title, image, content }) => (
+                    <Grid item md={2.8} key={id}>
+                        {' '}
+                        <ArticleListItem
+                            id={id}
+                            title={title}
+                            content={content}
+                            image={image}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    )
 }
 
 export default Favorite
